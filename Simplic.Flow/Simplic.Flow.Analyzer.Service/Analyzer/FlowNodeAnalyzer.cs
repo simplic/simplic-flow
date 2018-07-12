f04b5acd91abf304faad444021fac1e8665d0682
Yavuz Günay <yavuzgunay@gmail.com>
2018-07-12 18:00:35 +0200

    added analyzer

---
+++ Simplic.Flow/Simplic.Flow.Analyzer.Service/Analyzer/FlowNodeAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿namespace Simplic.Flow.Analyzer.Service
+{
+    public class FlowNodeAnalyzer : IFlowNodeAnalyzer
+    {
+        public bool Analyze(Node node)
+        {
+            return true; // analyzer the node somehow
+        }
+    }
+}
