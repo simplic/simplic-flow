7c4f955acf3ab075741055143b2a3953d9c55dad
Yavuz Günay <yavuzgunay@gmail.com>
2018-10-05 15:41:23 +0200

    added type to flow variable

--- src/Simplic.Flow.Configuration/Model/FlowVariableConfiguration.cs
+++ src/Simplic.Flow.Configuration/Model/FlowVariableConfiguration.cs
@@ -1,8 +1,11 @@
-﻿namespace Simplic.Flow.Configuration
+﻿using System;
+
+namespace Simplic.Flow.Configuration
 {
     public class FlowVariableConfiguration
     {
         public string Name { get; set; }
         public object Value { get; set; }
+        public Type Type { get; set; }
     }
 }
