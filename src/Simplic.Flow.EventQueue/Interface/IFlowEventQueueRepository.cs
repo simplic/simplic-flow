950c8a1dc2d34a522eb7e4356c1cdf2645c5f6cb
Yavuz Günay <yavuzgunay@gmail.com>
2019-04-01 09:53:29 +0200

    added clear event queue method to the repostiroy & service

--- src/Simplic.Flow.EventQueue/Interface/IFlowEventQueueRepository.cs
+++ src/Simplic.Flow.EventQueue/Interface/IFlowEventQueueRepository.cs
@@ -9,6 +9,7 @@ namespace Simplic.Flow.EventQueue
         IEnumerable<EventQueueModel> GetAllUnhandled();
         EventQueueModel Get(Guid id);
         bool Save(EventQueueModel model);
-        bool SetHandled(Guid id, bool isHandled);        
+        bool SetHandled(Guid id, bool isHandled);
+        void ClearEventQueue();
     }
 }
