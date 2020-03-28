493f2072ca442c055440d9f9e45d710ed2b4ca35
beggers <be@spiegelburg.net>
2020-03-29 00:13:33 +0100

    Add pipeline and update packages

--- src/Simplic.Flow.Configuration/package.bat
+++
@@ -1,3 +0,0 @@
-del *.nupkg
-nuget pack Simplic.Flow.Configuration.csproj -properties Configuration=Debug
-nuget push *.nupkg -Source http://simplic.biz:10380/nuget
--- src/Simplic.Flow.Log/package.bat
+++
@@ -1,3 +0,0 @@
-del *.nupkg
-nuget pack Simplic.Flow.Log.csproj -properties Configuration=Debug
-nuget push *.nupkg -Source http://simplic.biz:10380/nuget
