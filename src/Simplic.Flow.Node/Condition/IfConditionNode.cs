39e1c27134b7c58fcb0793c472d4cf6a35e757eb
Benedikt Eggers <be@spiegelburg.net>
2021-05-29 18:27:18 +0200

    .

--- src/Simplic.Flow.Node/Condition/IfConditionNode.cs
+++ src/Simplic.Flow.Node/Condition/IfConditionNode.cs
@@ -7,37 +7,11 @@ namespace Simplic.Flow.Node
     {
         public override bool Execute(IFlowRuntimeService runtime, DataPinScope scope)
         {
-            try
-            {
-                var value = scope.GetValue<object>(InPinToPrint);
-                System.Console.WriteLine(value);
-            }
-            catch (Exception ex)
-            {
-
-                throw;
-            }
-
-
-            if (OutNode != null)
-                runtime.EnqueueNode(OutNode, scope);
-
             return true;
         }
 
-        [FlowPinDefinition(DisplayName = "Out", Name = "OutNode", PinDirection = PinDirection.Out)]
-        public ActionNode OutNode { get; set; }
-
-        [DataPinDefinition(
-            Id = "b6ffc7b8-8f06-409c-8d27-7757518c2ab6",
-            ContainerType = DataPinContainerType.Single,
-            DataType = typeof(object),
-            Direction = PinDirection.In,
-            Name = "InPinToPrint",
-            DisplayName = "To Print")]
-        public DataPin InPinToPrint { get; set; }
-
         public override string FriendlyName { get { return nameof(IfConditionNode); } }
+
         public override string Name { get { return nameof(IfConditionNode); } }
     }
 }
