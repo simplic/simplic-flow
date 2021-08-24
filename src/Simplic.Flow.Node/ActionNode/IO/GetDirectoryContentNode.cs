d25e4a0efa8cc867a1e6f87fda544847e7e4b741
jason.gloger <jason.gloger@simplic.biz>
2021-08-24 09:51:28 +0200

    More properties to the flow node

--- src/Simplic.Flow.Node/ActionNode/IO/GetDirectoryContentNode.cs
+++ src/Simplic.Flow.Node/ActionNode/IO/GetDirectoryContentNode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Simplic.Flow.Node.IO
@@ -9,20 +10,24 @@ namespace Simplic.Flow.Node.IO
         public override bool Execute(IFlowRuntimeService runtime, DataPinScope scope)
         {
             var path = scope.GetValue<string>(InPinDirectoryPath);
+            var extensionPath = scope.GetValue<string>(InPinFileExtension);
+
             if (Directory.Exists(path))
             {
                 foreach (var file in Directory.GetFiles(path))
                 {
                     var childScope = scope.CreateChild();
+
                     childScope.SetValue(OutPinFilePath, file);
+                    childScope.SetValue(OutPinFileExtension, file.Substring(file.LastIndexOf(".") + 1));
+                    childScope.SetValue(OutPinFilePathWithoutFileName, file.Substring(0, file.LastIndexOf(@"\")));
+                    childScope.SetValue(OutPinFileName, file.Substring(file.LastIndexOf(@"\") + 1, file.LastIndexOf(".") - file.LastIndexOf(@"\") - 1));
 
                     if (OutNodeEachFile != null)
                         runtime.EnqueueNode(OutNodeEachFile, childScope);
                 }
-
                 if (OutNodeCompleted != null)
                     runtime.EnqueueNode(OutNodeCompleted, scope);
-
                 return true;
             }
             else
@@ -31,6 +36,27 @@ namespace Simplic.Flow.Node.IO
                 return false;
             }
         }
+
+        [FlowPinDefinition(DisplayName = "Check Directory", Name = "InNodeCheckDirectory", PinDirection = PinDirection.In)]
+        public ActionNode InNodeCheckDirectory { get; set; }
+
+        [DataPinDefinition(
+            Id = "488f578c-d77c-4bca-80e8-8c456208d8c8",
+            ContainerType = DataPinContainerType.Single,
+            DataType = typeof(string),
+            Direction = PinDirection.In,
+            Name = "InPinDirectoryPath",
+            DisplayName = "Directory Path")]
+        public DataPin InPinDirectoryPath { get; set; }
+
+        [DataPinDefinition(
+            Id = "b405732e-089e-4a20-b002-281721909629",
+            ContainerType = DataPinContainerType.List,
+            DataType = typeof(List<string>),
+            Direction = PinDirection.In,
+            Name = "InPinFileExtension",
+            DisplayName = "File Extension")]
+        public DataPin InPinFileExtension { get; set; }
 
         [FlowPinDefinition(DisplayName = "Each File", Name = "OutNodeEachFile", PinDirection = PinDirection.Out)]
         public ActionNode OutNodeEachFile { get; set; }
@@ -48,13 +74,31 @@ namespace Simplic.Flow.Node.IO
         public DataPin OutPinFilePath { get; set; }
 
         [DataPinDefinition(
-            Id = "488f578c-d77c-4bca-80e8-8c456208d8c8",
+            Id = "fec0c781-45e9-4d4b-a6a6-90a759690bc2",
             ContainerType = DataPinContainerType.Single,
             DataType = typeof(string),
-            Direction = PinDirection.In,
-            Name = "InPinDirectoryPath",
-            DisplayName = "Directory Path")]
-        public DataPin InPinDirectoryPath { get; set; }
+            Direction = PinDirection.Out,
+            Name = "OutPinFilePathWithoutFileName",
+            DisplayName = "File Path without File Name")]
+        public DataPin OutPinFilePathWithoutFileName { get; set; }
+
+        [DataPinDefinition(
+            Id = "d5ce2a7f-4710-455b-99c5-7b92ef2f71d7",
+            ContainerType = DataPinContainerType.Single,
+            DataType = typeof(string),
+            Direction = PinDirection.Out,
+            Name = "OutPinFileName",
+            DisplayName = "File Name")]
+        public DataPin OutPinFileName { get; set; }
+
+        [DataPinDefinition(
+            Id = "d85c387e-61fe-40fa-a59b-88eb117e21d1",
+            ContainerType = DataPinContainerType.Single,
+            DataType = typeof(string),
+            Direction = PinDirection.Out,
+            Name = "OutPinFileNameExtension",
+            DisplayName = "File Name Extension")]
+        public DataPin OutPinFileExtension { get; set; }
 
         public override string FriendlyName
         {
