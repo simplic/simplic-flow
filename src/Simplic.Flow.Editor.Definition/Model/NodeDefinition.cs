c7ff599eef2de5fae01d376b1369ecb95655ae33
Benedikt Eggers <be@spiegelburg.net>
2022-03-08 13:52:04 +0100

    Update src/Simplic.Flow.Editor.Definition/Model/NodeDefinition.cs

--- src/Simplic.Flow.Editor.Definition/Model/NodeDefinition.cs
+++ src/Simplic.Flow.Editor.Definition/Model/NodeDefinition.cs
@@ -12,6 +12,7 @@ namespace Simplic.Flow.Editor.Definition
         /// Gets or sets the node documentation URL.
         /// </summary>
         public string DocumentationUrl { get; set; }
+
         /// <summary>
         /// Gets or sets the full type name of the node.
         /// <para>The full type name is used to try to dynamically create the node documentation URL if not explicitly given as an attribute.</para>
